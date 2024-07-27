514a2ec6cf784ae2f0e9eeb5c6e5813f72c6c05d
Ali Hasan <ahb7671@gmail.com>
2024-07-27 10:28:08 +0500

    include signing keys
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- product.mk
+++ product.mk
@@ -11,3 +11,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/feather/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
+
+# keys
+include certs/keys.mk
