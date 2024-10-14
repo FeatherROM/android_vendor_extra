rn124g-development/vendor_extra

vendor: add updater
Signed-off-by: Ali Hasan <ahb7671@gmail.com>

product.mk
@@ -4,3 +4,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     CertifiedPropsOverlay
+
+# Updater
+PRODUCT_PACKAGES += \
+    Updates
+
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc

FeatherROM/android_vendor_extra

product.mk
@@ -4,3 +4,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     CertifiedPropsOverlay
+
+# Updater
+PRODUCT_PACKAGES += \
+    Updates
+
+PRODUCT_COPY_FILES += \
+    vendor/feather/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc